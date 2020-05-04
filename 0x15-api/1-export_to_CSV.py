a19d3c3d9026d204becb47706ba5dd099440df19
JDorangetree <julian.naranjo2014@gmail.com>
2020-05-04 17:23:56 -0500

    [Fix] QUOTE ALL

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,20 +6,19 @@ import os
 
 if __name__ == "__main__":
     user_id = argv[1]
-    file = open("{}.csv".format(user_id), "w")
     user_dict = requests.get('https://jsonplaceholder.typicode.com/users/{}'
                              .format(user_id)).json()
-    name = user_dict["name"]
+    name = user_dict["username"]
     list_of_dict = requests.get('https://jsonplaceholder.\
 typicode.com/users/{}/todos'.format(user_id)).json()
     rows = []
     for item in list_of_dict:
-        user_id = item["userId"]
-        completed = item["completed"]
+        user_id = str(item["userId"])
+        completed = str(item["completed"])
         title = item["title"]
-        row = [user_id, completed, title]
+        row = [user_id, name, completed, title]
         rows.append(row)
-    with file:
-        writer = csv.writer(file)
-        for item in rows:
-            writer.writerow(item)
+    with open("{}.csv".format(user_id), "w") as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',',
+                                quotechar='"', quoting=csv.QUOTE_ALL)
+        spamwriter.writerows(rows)
