163a83927f22b7a696380b3d46a974ec60021731
JDorangetree <julian.naranjo2014@gmail.com>
2020-05-07 19:15:29 -0500

    [Fix] .json

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,10 +8,12 @@ def number_of_subscribers(subreddit):
 
     url = 'https://www.reddit.com//r/{}/about.json'.format(subreddit)
     user_agent = {"User-Agent": "myredditapp:v1.2.3"}
-    subreddit_dict = get(url, headers=user_agent, allow_redirects=False).json()
+    subreddit_dict = get(url, headers=user_agent, allow_redirects=False)
 
     if subreddit_dict.status_code != 200:
         return 0
+
+    subreddit_dict = subreddit_dict.json()
 
     data = subreddit_dict.get("data")
     subscribers = data["subscribers"]
